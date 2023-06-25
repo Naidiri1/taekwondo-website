792b110a1482bcbda0636f35953133fad9d0e327
Iris <iridian95@live.com>
2023-06-25 16:17:02 -0400

    adding card

--- src/components/Main/cards.js
+++ src/components/Main/cards.js
@@ -7,8 +7,8 @@ import Camps from "../../assets/imgs/camps.jpg";
 const cards = [
   {
     img: kids,
-    cardTitle: "4-7 year olds (HODORI)",
-    cardDescription: "class starts at 8 am",
+    cardTitle: "HODORI 4-7 yrs",
+    cardDescription: "Class starts at 5:45pm",
     modalTitle: "class 1",
     modalContent:
       "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
@@ -16,35 +16,8 @@ const cards = [
   },
   {
     img: beginner,
-    cardTitle: "8 & UP Beginner Class",
-    cardDescription: "class starts at 8 am",
-    modalTitle: "class 1",
-    modalContent:
-      "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
-    modalImgApp: belt,
-  },
-  {
-    img: advanced,
-    cardTitle: "8 & Up Advance Classes",
-    cardDescription: "class starts at 8 am",
-    modalTitle: "class 1",
-    modalContent:
-      "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
-    modalImgApp: belt,
-  },
-  {
-    img: afterSchool,
-    cardTitle: "Afterschool Program",
-    cardDescription: "class starts at 8 am",
-    modalTitle: "class 1",
-    modalContent:
-      "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
-    modalImgApp: belt,
-  },
-  {
-    img: Camps,
-    cardTitle: "Taekwondo Camps",
-    cardDescription: "class starts at 8 am",
+    cardTitle: "8 yrs & Up Beginner/Advanced Class",
+    cardDescription: "Class starts at 6:15pm",
     modalTitle: "class 1",
     modalContent:
       "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
@@ -53,12 +26,31 @@ const cards = [
   {
     img: belt,
     cardTitle: "Adult program",
-    cardDescription: "class starts at 8 am",
+    cardDescription: "Class starts at 7:00pm",
     modalTitle: "class 1",
     modalContent:
       "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
     modalImgApp: belt,
   },
+  {
+    img: afterSchool,
+    cardTitle: "Afterschool Program",
+    cardDescription: "Starts at 3:00pm",
+    modalTitle: "class 1",
+    modalContent:
+      "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
+    modalImgApp: belt,
+  },
+  {
+    img: Camps,
+    cardTitle: "Taekwon-Do Camps",
+    cardDescription: "Starts at 7:30am",
+    modalTitle: "class 1",
+    modalContent:
+      "Allows children to feel engaged with our martial arts program by providing strong visual cues to help them align with their achievements and goals.",
+    modalImgApp: belt,
+  },
+  
 ];
 
 export default cards;
